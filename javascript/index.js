70d6d6ff35e3b866489e21c8bbff9c3a621a64a2
abdelrhman mahmoud shibl <abdulrhman.mahmoud44@gmail.com>
2023-05-21 10:11:45 +0300

    organise the code

--- javascript/index.js
+++ javascript/index.js
@@ -1,41 +1,41 @@
-$(document).ready(function() {
-    // Retrieve the active link from session storage, if available
-    var activeLink = sessionStorage.getItem('activeLink');
+$(document).ready(function () {
+  // Retrieve the active link from session storage, if available
+  var activeLink = sessionStorage.getItem('activeLink');
 
-    // Add active class to the stored active link, if available
-    if (activeLink) {
-      $('a.nav-link[href="' + activeLink + '"]').addClass('active');
-    }
+  // Add active class to the stored active link, if available
+  if (activeLink) {
+    $('a.nav-link[href="' + activeLink + '"]').addClass('active');
+  }
   // Attach click event handler to navigation links
-  $('a.nav-link').click(function(e) {
+  $('a.nav-link').click(function (e) {
     e.preventDefault(); // Prevent default link behavior
 
     var url = $(this).attr('href'); // Get the URL of the linked page
-  // Remove active class from all navigation links
-  $('a.nav-link').removeClass('active');
-    
-  // Add active class to the clicked navigation link
-  $(this).addClass('active');
+    // Remove active class from all navigation links
+    $('a.nav-link').removeClass('active');
 
-      // Use the History API to update the URL
-      history.pushState(null, null, url);
+    // Add active class to the clicked navigation link
+    $(this).addClass('active');
+
+    // Use the History API to update the URL
+    history.pushState(null, null, url);
 
 
-     // Store the active link in session storage
-  sessionStorage.setItem('activeLink', url);
+    // Store the active link in session storage
+    sessionStorage.setItem('activeLink', url);
     // Make an AJAX request to load the content of the linked page
-      $.ajax({
-        url: url,
-        method: 'GET',
-        dataType: 'html',
-        success: function(response) {
-          // Replace the content of the current page with the loaded content
-          $('body').html(response);
-        },
-        error: function(xhr, status, error) {
-          console.log(error);
-        }
-      });
+    $.ajax({
+      url: url,
+      method: 'GET',
+      dataType: 'html',
+      success: function (response) {
+        // Replace the content of the current page with the loaded content
+        $('body').html(response);
+      },
+      error: function (xhr, status, error) {
+        console.log(error);
+      }
+    });
   })
 });
 
